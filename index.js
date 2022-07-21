d583dc350672b9d8774287dc24b627b60809a350
asurowy <101614024+asurowy@users.noreply.github.com>
2022-07-21 16:30:58 -0400

    commit tree 8e0a738301496c88532eaa66bb1a1c29c5264b61

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2 
+
+let random = Math.round(Math.random() * 10)+ 1;
+
+let num3 = 100;
+let num4 = 16;
+
+let mod = num3 % num4;
+
+let max= Math.max( 1,2,3,4,20);
